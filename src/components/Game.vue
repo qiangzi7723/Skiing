ebaa6421b343d2da751fac74894b9e33d03b96a5
qq1092672755@gmail.com <Mm364639010>
2017-12-31 00:13:34 +0800

    推送

--- src/components/Game.vue
+++ src/components/Game.vue
@@ -7,21 +7,25 @@
 <script>
     import Game from '../game/index';
     export default {
-        mounted(){
-            const game=new Game();
+        mounted() {
+            const game = new Game();
             game.init();
         }
     };
+
 </script>
 
 <style lang='scss'>
-@import '../assets/scss/extend.scss';
-.game-wrap{
-    width: 100vw;
-    height: 100vh;
-    #canvas{
+    @import '../assets/scss/extend.scss';
+    .game-wrap {
         width: 100vw;
         height: 100vh;
+        #canvas {
+            width: 100vw;
+            height: 100vh;
+            canvas {
+            }
+        }
     }
-}
+
 </style>
--- src/game/index.js
+++ src/game/index.js
@@ -1,4 +1,4 @@
-let game,help;
+let game,help,ground,role;
 
 class Game {
     constructor() {}
@@ -15,6 +15,7 @@ class Game {
         game = new Phaser.Game(this.w, this.h, Phaser.WEBGL, 'canvas', {
             preload: this.preload.bind(this), // 重新绑定this指向
             create: this.create.bind(this),
+            render:this.render.bind(this)
         });
 
         game.state.start('preload');
@@ -26,6 +27,7 @@ class Game {
         game.load.image('role', require('@/assets/img/role.png'));
         game.load.image('bg', require('@/assets/img/bg.png'));
         game.load.image('tileBg', require('@/assets/img/bga.png'));
+        game.load.image('ground',require('@/assets/img/ground.png'));
 
         game.load.onLoadComplete.add(() => {
             game.state.start('create');
@@ -33,22 +35,59 @@ class Game {
     }
 
     create() {
-        const tileBg = game.add.image(0, 0, 'tileBg');
-        help.widthSize(tileBg);
-        const role = new Role();
+        game.physics.startSystem(Phaser.Physics.ARCADE);
+
+        const tileBgHack=game.cache.getImage('tileBg');
+        const s=this.w / tileBgHack.width;
+        const tileBg = game.add.tileSprite(0, 0, this.w,this.h,'tileBg');
+        tileBg.tileScale.setTo(s, s);
+        tileBg.autoScroll(-100,0)
+        // const role = new Role();
+
+        new Ground();
+        new Role();
+    }
+
+    render(){
+        game.debug.body(ground);
+        game.debug.bodyInfo(role, 32, 32);
+        game.debug.body(role);
     }
 
 }
 
+class Ground{
+    constructor(){
+        ground=game.add.tileSprite(0,300,game.world.width,100,'ground');
+        game.physics.enable(ground, Phaser.Physics.ARCADE); //开启地面的物理系统
+        ground.body.immovable = true; //让地面在物理环境中固定不动
+        ground.rotation=0.1;
+        ground.autoScroll(-100,0)
+        console.log(ground);
+    }
+}
+
 class Role {
     constructor(){
-        this.role=game.add.image(0, 0, 'role');
+        // this.role=new Phaser.Image(game,0, 0, 'role');
+        // this.role=game.add.image();
+        // console.log(game.stage.add(this.role,-1));
+        // game.world.rotation = 0.1;
+        // game.stage.rotation=0.1;
+        // console.log(game.stage)
+        role=game.add.sprite(0,0,'role');
         this.initPhysics();
+        game.stage.backgroundColor = '#124184';
+
     }
     initPhysics() {
-        game.physics.enable(this.role, Phaser.Physics.ARCADE);
+        game.physics.enable(role, Phaser.Physics.ARCADE);
         // console.log(this.role)
-        // this.role.body.velocity.x=1;
+        // this.role.body.velocity.x=10;
+        role.body.setSize(0,0,100,330)
+        role.body.gravity.y=100;
+        role.rotation=0.1;
+        role.body.collideWorldBounds = true;
     }
 }
 
